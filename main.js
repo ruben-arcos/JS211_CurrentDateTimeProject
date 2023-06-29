be3ea32e797d971d4eda0e39da8bc7442c4850b4
Ruben Arcos <ruben.arcos06@gmail.com>
2023-06-28 19:45:34 -0500

    completed js functions to make per homework instructions

--- main.js
+++ main.js
@@ -13,38 +13,74 @@ const displayDate = () => {
   document.getElementById("display-element").innerHTML = currentDate;
 }
  
+// ******************************************************************************
 
 // Write a JavaScript program to convert a number to a string.
+let num = 4;
+let str = String(num);
 
+console.log(str);
 
 
 // Write a JavaScript program to convert a string to the number.
+let string = '10';
+let number = Number(string);
 
+console.log(number);
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
+
+const theWorldIsRound = true;
+const name = null;
+let doughtnutFlavor
+let integer = 4;
+let notANumber = 'abd'/3
+let string5 = 'sunshine'
+
+console.log(typeof theWorldIsRound)// * Boolean
+console.log(typeof name)// * Null
+console.log(typeof doughtnutFlavor)// * Undefined
+console.log(typeof integer)// * Number
+console.log(notANumber)// * NaN
+console.log(typeof string5)// * String
   
 
-  
 // Write a JavaScript program that adds 2 numbers together.
-
-
+let addition = 2 + 4;
+console.log(addition);
 
 // Write a JavaScript program that runs only when 2 things are true.
+const condition1 = true;
+const condition2 = true;
 
+if (condition1 && condition2 === true) {
+  console.log('both conditions are true. running the program')
+} else {
+  console.log('not true. program run fail!')
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+const condition3 = true;
+const condition4 = false;
 
-
+if (condition3 || condition4) {
+  console.log('program running')
+} else {
+  console.log('program not running')
+}
 
 // Write a JavaScript program that runs when both things are not true.  
+const condition5 = false;
+const condition6 = false;
+
+if (!condition5 && !condition6) {
+  console.log('program running')
+} else {
+  console.log('program stopped')
+}
+
 
 // ***************************
 //         PART TWO
@@ -65,3 +101,6 @@ const displayDate = () => {
 // Video1: https://player.vimeo.com/video/377147232
 // Video2: https://www.youtube.com/embed/bkvH28PXLWc
 // Video3: https://www.youtube.com/embed/TrGI9Yki-24
+
+
+
